168530c2845aa94cd7a67e1f6453b2d36529db99
Megan0814 <megan.gouder.i80710@mcast.edu.mt>
2025-12-03 16:39:00 +0100

    Added prefab folder and Waypoints for cloud enemy
    made it so the first enemy moves on a path and added a prefab folder

---
+++ MeganGouder_MSD_4.2A/Assets/Scripts/enemyPath.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class enemyPath : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 6f;
+
+    int waypointIndex = 0;
+    //shows the next waypoint that the enemy would want to go
+    
+    void Start()
+    {
+      transform.position = waypoints[waypointIndex].transform.position;
+      //makes sure the enemy starts is the first waypoint  
+    }
+    void Update()
+    {
+        EnemyMove();
+    }
+
+    void EnemyMove()
+    {
+        if (waypointIndex <= waypoints.Count -1)
+        {
+            //target position is where we want it to go
+            var targetPosition = waypoints[waypointIndex].transform.position;
+
+            targetPosition.z = 0f;
+            //to make sure the z position is zero
+
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+            if (waypointIndex >= waypoints.Count)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
+}
